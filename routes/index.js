274e06b4a7d9aa4d90df31959df96ac3ebbf7f74
LRAstarter <jevin.chen1@ontartiotechu.net>
2022-10-30 22:30:48 -0400

    First commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+
+router.get('/blog', function(req, res, next) {
+  res.render('index', { title: 'Blog' });
+});
+
+
+module.exports = router;
